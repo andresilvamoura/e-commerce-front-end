9ce5d618161f0fdcbd3cc9ce86ace7204560f8b2
Andre Moura <andremoura500@gmail.com>
2022-05-26 20:40:04 -0300

    feat: Criado a estrutura dos componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BookstoreComponent } from './components/bookstore/bookstore.component';
+import { FiltersComponent } from './components/bookstore/filters/filters.component';
+import { ProductListComponent } from './components/bookstore/product-list/product-list.component';
+import { ProductItemComponent } from './components/bookstore/product-list/product-item/product-item.component';
+import { HeaderComponent } from './components/shared/header/header.component';
+import { NavComponent } from './components/shared/nav/nav.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BookstoreComponent,
+    FiltersComponent,
+    ProductListComponent,
+    ProductItemComponent,
+    HeaderComponent,
+    NavComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
